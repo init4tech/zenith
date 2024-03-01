647dc6f249ebe618ddc709302494347985fd46d9
Anna Carroll <anna.s.carroll@gmail.com>
2024-03-01 16:36:06 +0000

    deploy script

---
+++ script/Zenith.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {Zenith} from "../src/Zenith.sol";
+
+contract DeployZenith is Script {
+    // deploy: forge script DeployZenith --sig "run()" --rpc-url $RPC --etherscan-api-key $ETHERSCAN_API_KEY --private-key $RAW_PRIVATE_KEY --broadcast --verify
+    function run() public {
+        vm.broadcast();
+        new Zenith{salt: "zenith"}();
+    }
+}
