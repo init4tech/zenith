70a0e62ced2725dbc0fb0ace2e86b7a5d74fdec8
Anna Carroll <anna.s.carroll@gmail.com>
2024-03-06 15:38:04 +0000

    add no verify script

--- script/Zenith.s.sol
+++ script/Zenith.s.sol
@@ -6,6 +6,8 @@ import {Zenith} from "../src/Zenith.sol";
 
 contract DeployZenith is Script {
     // deploy: forge script DeployZenith --sig "run()" --rpc-url $RPC --etherscan-api-key $ETHERSCAN_API_KEY --private-key $RAW_PRIVATE_KEY --broadcast --verify
+
+    // deploy NO VERIFY: forge script DeployZenith --sig "run()" --rpc-url $RPC --private-key $RAW_PRIVATE_KEY --broadcast
     function run() public {
         vm.broadcast();
         new Zenith{salt: "zenith"}();
