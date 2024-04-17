617b96fbba679090604f12a0e9cb59f1f9b131e8
Anna Carroll <anna.s.carroll@gmail.com>
2024-04-17 16:11:35 -0500

    rm create2

--- script/Zenith.s.sol
+++ script/Zenith.s.sol
@@ -9,6 +9,6 @@ contract DeployZenith is Script {
     // forge script DeployZenith --sig "run()" --rpc-url $RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY --private-key $PRIVATE_KEY --broadcast --verify
     function run() public {
         vm.broadcast();
-        new Zenith{salt: "zenith-3"}(msg.sender);
+        new Zenith(msg.sender);
     }
 }
