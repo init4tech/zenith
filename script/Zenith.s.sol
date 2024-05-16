5771389f57965efaca59c85727f92fe62f4b982c
Anna Carroll <anna.s.carroll@gmail.com>
2024-05-16 16:47:14 +0100

    feat: configure sequencer key at deploy time

--- script/Zenith.s.sol
+++ script/Zenith.s.sol
@@ -6,9 +6,11 @@ import {Zenith} from "../src/Zenith.sol";
 
 contract DeployZenith is Script {
     // deploy:
-    // forge script DeployZenith --sig "run()" --rpc-url $RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY --private-key $PRIVATE_KEY --broadcast --verify
-    function run() public {
-        vm.broadcast();
-        new Zenith(block.chainid + 1, msg.sender);
+    // forge script DeployZenith --sig "run(address)" --rpc-url $RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY --private-key $PRIVATE_KEY --broadcast --verify $SEQUENCER_KEY
+    function run(address sequencer) public {
+        vm.startBroadcast();
+        Zenith z = new Zenith(block.chainid + 1, msg.sender);
+        z.grantRole(z.SEQUENCER_ROLE(), sequencer);
+        payable(address(z)).send(0.0123 ether);
     }
 }
