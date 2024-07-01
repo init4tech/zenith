f029900693555acc23ff5a77487207a32f6d96d9
Anna Carroll <anna.s.carroll@gmail.com>
2024-07-01 16:36:22 +0200

    feat: test zenith sequencer fns

--- test/Zenith.t.sol
+++ test/Zenith.t.sol
@@ -23,6 +23,8 @@ contract ZenithTest is Test {
         bytes32 blockDataHash
     );
 
+    event SequencerSet(address indexed sequencer, bool indexed permissioned);
+
     function setUp() public {
         address[] memory initialEnterTokens;
         target = new Zenith(block.chainid + 1, address(this), initialEnterTokens, address(this));
@@ -39,13 +41,18 @@ contract ZenithTest is Test {
         commit = target.blockCommitment(header);
     }
 
+    function test_setUp() public {
+        assertEq(target.sequencerAdmin(), address(this));
+        assertEq(target.deployBlockNumber(), block.number);
+    }
+
     // cannot submit block with incorrect host block number
     function test_incorrectHostBlock() public {
         // change to incorrect host block number
         header.hostBlockNumber = 100;
         commit = target.blockCommitment(header);
 
-        // sign block commitmenet with sequencer key
+        // sign block commitment with sequencer key
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(sequencerKey, commit);
 
         vm.expectRevert(Zenith.IncorrectHostBlock.selector);
@@ -54,8 +61,10 @@ contract ZenithTest is Test {
 
     // can submit block successfully with acceptable header & correct signature provided
     function test_submitBlock() public {
-        // sign block commitmenet with correct sequencer key
+        // sign block commitment with correct sequencer key
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(sequencerKey, commit);
+
+        assertEq(target.lastSubmittedAtBlock(header.rollupChainId), 0);
 
         // should emit BlockSubmitted event
         vm.expectEmit();
@@ -63,11 +72,13 @@ contract ZenithTest is Test {
             vm.addr(sequencerKey), header.rollupChainId, header.gasLimit, header.rewardAddress, header.blockDataHash
         );
         target.submitBlock(header, v, r, s, blockData);
+
+        assertEq(target.lastSubmittedAtBlock(header.rollupChainId), block.number);
     }
 
     // cannot submit block with invalid sequencer signer from non-permissioned key
     function test_notSequencer() public {
-        // sign block commitmenet with NOT sequencer key
+        // sign block commitment with NOT sequencer key
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(notSequencerKey, commit);
 
         vm.expectRevert(abi.encodeWithSelector(Zenith.BadSignature.selector, vm.addr(notSequencerKey)));
@@ -76,7 +87,7 @@ contract ZenithTest is Test {
 
     // cannot submit block with sequencer signature over different block header data
     function test_badSignature() public {
-        // sign original block commitmenet with sequencer key
+        // sign original block commitment with sequencer key
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(sequencerKey, commit);
 
         // change header data from what was signed by sequencer
@@ -90,7 +101,7 @@ contract ZenithTest is Test {
 
     // cannot submit two rollup blocks within one host block
     function test_onePerBlock() public {
-        // sign block commitmenet with correct sequencer key
+        // sign block commitment with correct sequencer key
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(sequencerKey, commit);
 
         // should emit BlockSubmitted event
@@ -104,5 +115,49 @@ contract ZenithTest is Test {
         // (NOTE: this test works without changing the Header because forge does not increment block.number when it mines a transaction)
         vm.expectRevert(Zenith.OneRollupBlockPerHostBlock.selector);
         target.submitBlock(header, v, r, s, blockData);
+    }
+
+    function test_addSequencer() public {
+        address newSequencer = vm.addr(notSequencerKey);
+        assertFalse(target.isSequencer(newSequencer));
+
+        vm.expectEmit();
+        emit SequencerSet(newSequencer, true);
+        target.addSequencer(newSequencer);
+
+        assertTrue(target.isSequencer(newSequencer));
+
+        // can sign block now with new sequencer key
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(notSequencerKey, commit);
+        vm.expectEmit();
+        emit BlockSubmitted(
+            newSequencer, header.rollupChainId, header.gasLimit, header.rewardAddress, header.blockDataHash
+        );
+        target.submitBlock(header, v, r, s, blockData);
+    }
+
+    function test_removeSequencer() public {
+        address sequencer = vm.addr(sequencerKey);
+        assertTrue(target.isSequencer(sequencer));
+
+        vm.expectEmit();
+        emit SequencerSet(sequencer, false);
+        target.removeSequencer(sequencer);
+
+        assertFalse(target.isSequencer(sequencer));
+
+        // cannot sign block with the sequencer key
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(sequencerKey, commit);
+        vm.expectRevert(abi.encodeWithSelector(Zenith.BadSignature.selector, vm.addr(sequencerKey)));
+        target.submitBlock(header, v, r, s, blockData);
+    }
+
+    function test_notSequencerAdmin() public {
+        vm.startPrank(address(0x01));
+        vm.expectRevert(Zenith.OnlySequencerAdmin.selector);
+        target.addSequencer(address(0x02));
+
+        vm.expectRevert(Zenith.OnlySequencerAdmin.selector);
+        target.removeSequencer(address(0x02));
     }
 }
