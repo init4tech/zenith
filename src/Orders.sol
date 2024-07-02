e00486ea4b857279cfbbb91e5269eb1a102a536c
Anna Carroll <anna.s.carroll@gmail.com>
2024-07-02 14:02:46 +0200

    move structs to top of file

--- src/Orders.sol
+++ src/Orders.sol
@@ -3,6 +3,29 @@ pragma solidity ^0.8.24;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
+/// @notice Tokens sent by the swapper as inputs to the order
+/// @dev From ERC-7683
+struct Input {
+    /// @dev The address of the ERC20 token on the origin chain
+    address token;
+    /// @dev The amount of the token to be sent
+    uint256 amount;
+}
+
+/// @notice Tokens that must be receive for a valid order fulfillment
+/// @dev From ERC-7683
+struct Output {
+    /// @dev The address of the ERC20 token on the destination chain
+    /// @dev address(0) used as a sentinel for the native token
+    address token;
+    /// @dev The amount of the token to be sent
+    uint256 amount;
+    /// @dev The address to receive the output tokens
+    address recipient;
+    /// @dev The destination chain for this output
+    uint32 chainId;
+}
+
 /// @notice Contract capable of processing fulfillment of intent-based Orders.
 abstract contract OrderDestination {
     /// @notice Emitted when an Order's Output is sent to the recipient.
@@ -34,29 +57,6 @@ abstract contract OrderDestination {
 
 /// @notice Contract capable of registering initiation of intent-based Orders.
 abstract contract OrderOrigin {
-    /// @notice Tokens sent by the swapper as inputs to the order
-    /// @dev From ERC-7683
-    struct Input {
-        /// @dev The address of the ERC20 token on the origin chain
-        address token;
-        /// @dev The amount of the token to be sent
-        uint256 amount;
-    }
-
-    /// @notice Tokens that must be receive for a valid order fulfillment
-    /// @dev From ERC-7683
-    struct Output {
-        /// @dev The address of the ERC20 token on the destination chain
-        /// @dev address(0) used as a sentinel for the native token
-        address token;
-        /// @dev The amount of the token to be sent
-        uint256 amount;
-        /// @dev The address to receive the output tokens
-        address recipient;
-        /// @dev The destination chain for this output
-        uint32 chainId;
-    }
-
     /// @notice Thrown when an Order is submitted with a deadline that has passed.
     error OrderExpired();
 
